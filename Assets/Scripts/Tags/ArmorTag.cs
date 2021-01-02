eb53b181bd8aaf4b6ed8e11491ab376e4986548c
Marcel <marcelurpi@gmail.com>
2021-01-02 17:39:27 +0100

    Classes Tag i derivades

---
+++ Assets/Scripts/Tags/ArmorTag.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ArmorTag : Tag
+{
+    [SerializeField] private int baseArmor;
+
+    public int GetBaseArmor() => baseArmor;
+}
---
+++ Assets/Scripts/Tags/AttackTag.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class AttackTag : Tag
+{
+    [SerializeField] private int baseDamage;
+
+    public int GetBaseDamage() => baseDamage;
+}
