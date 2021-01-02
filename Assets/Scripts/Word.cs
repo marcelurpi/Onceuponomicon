290dff7ff59d642609a04b7477a125d1006950ac
Marcel <marcelurpi@gmail.com>
2021-01-02 17:43:41 +0100

    Classe Word
    Afegir Paraules
    
    Changes

---
+++ Assets/Scripts/Word.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Word : ScriptableObject
+{
+    [SerializeField] private bool singleUse;
+    [SerializeField] private string word;
+    [SerializeField] private Tag[] tags;
+
+    public bool IsSingleUse() => singleUse;
+    public string GetWord() => word;
+    public Tag[] GetTags() => tags;
+}
