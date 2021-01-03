5cc99ae671e972dc3e241ef6c754598ce23eb5aa
Marcel <marcelurpi@gmail.com>
2021-01-03 17:06:38 +0100

    Classe Page + implementar rendering

---
+++ Assets/Scripts/Page.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using TMPro;
+using UnityEngine;
+
+public class Page : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI textMesh;
+    [SerializeField] private PageText pageText;
+
+    private void Start()
+    {
+        RenderPageText();
+    }
+
+    private void OnValidate()
+    {
+        pageText.OnValidate();
+    }
+
+    [ContextMenu("RenderPageText")]
+    private void RenderPageText()
+    {
+        StringBuilder builder = new StringBuilder();
+        foreach (PageTextModule module in pageText.GetModules())
+        {
+            switch (module.GetModuleType())
+            {
+                case PageTextModule.Type.Text:
+                    builder.Append(module.GetText());
+                    break;
+                case PageTextModule.Type.Word:
+                    builder.Append("<b>").Append(module.GetWord().GetWord()).Append("</b>");
+                    break;
+                case PageTextModule.Type.Gap:
+                    builder.Append("_____");
+                    break;
+                default:
+                    break;
+            }
+        }
+        textMesh.text = builder.ToString();
+    }
+}
