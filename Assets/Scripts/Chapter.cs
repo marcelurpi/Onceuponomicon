b8619cff3c0b24c320dba73312e670941a6bf9a4
Marcel <marcelurpi@gmail.com>
2021-01-04 19:51:34 +0100

    Classe Chapter

---
+++ Assets/Scripts/Chapter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Chapter : ScriptableObject
+{
+    [SerializeField] private PageText[] pages;
+    [SerializeField] private Chapter nextChapter;
+
+    public PageText[] GetPages() => pages;
+    public Chapter GetNextChapter() => nextChapter;
+}
